3e542647504a5db1839d8929228674cba2ed3a4c
zelkovaria <hyein396@gmail.com>
2024-10-24 17:49:07 +0900

    feat: WorryContent 컴포넌트 구현 및 css 적용 - #22

--- src/features/detailCommunity/components/WorryContent.tsx
+++ src/features/detailCommunity/components/WorryContent.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import styled from 'styled-components';
+import DetailPostContent from './DetailPostContent';
 
 const WorryContent = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <h2>고민</h2>
+      <DetailPostContent />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  h2 {
+    font-size: 20px;
+    font-weight: 600;
+    color: #ffffff;
+    margin-left: 4px;
+  }
+`;
 
 export default WorryContent;
--- src/features/detailCommunity/components/index.ts
+++ src/features/detailCommunity/components/index.ts
@@ -1,1 +1,2 @@
 export { default as WorryContent } from './WorryContent';
+export { default as DetailPostContent } from './DetailPostContent';
