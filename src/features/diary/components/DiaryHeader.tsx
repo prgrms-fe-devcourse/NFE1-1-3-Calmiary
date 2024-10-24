3da2048f0a80af78b10e87ef2454966eae4ce167
Gose <Winter-Climbing@github.com>
2024-10-24 10:14:50 +0900

    feat: Diary Header UI 구현

--- src/features/diary/components/DiaryHeader.tsx
+++ src/features/diary/components/DiaryHeader.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
 function DiaryHeader() {
-  return <header>다이어리</header>;
+  return <S_DiaryHeader>다이어리</S_DiaryHeader>;
 }
 
 export default DiaryHeader;
+
+const S_DiaryHeader = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 20px 0px;
+  color: white;
+  margin-bottom: 50px;
+`;
