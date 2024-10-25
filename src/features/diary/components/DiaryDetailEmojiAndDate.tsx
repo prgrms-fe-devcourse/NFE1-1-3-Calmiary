ce237c43ed1feb750d9d93174025a78e7f7471a2
Gose <Winter-Climbing@github.com>
2024-10-25 10:50:19 +0900

    feat: 이모지 UI / 날짜 구현

--- src/features/diary/components/DiaryDetailEmojiAndDate.tsx
+++ src/features/diary/components/DiaryDetailEmojiAndDate.tsx
@@ -1,5 +1,27 @@
+import styled from 'styled-components';
+import { Icon } from '../../../components/ui/Icon';
+
 function DiaryDetailEmojiAndDate() {
-  return <div>DiaryDetailEmojiAndData</div>;
+  return (
+    <S_DiaryDetailEmojiAndDateWrapper>
+      <Icon type="emoji_smail" size={55} />
+      <p>24년 10월 22일의 고민</p>
+    </S_DiaryDetailEmojiAndDateWrapper>
+  );
 }
 
 export default DiaryDetailEmojiAndDate;
+
+const S_DiaryDetailEmojiAndDateWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 8.33dvh;
+  margin-bottom: 20px;
+  font-size: 14px;
+
+  p {
+    margin-top: 18px;
+  }
+`;
--- src/features/diary/pages/DiaryDetailPage.tsx
+++ src/features/diary/pages/DiaryDetailPage.tsx
@@ -1,5 +1,25 @@
+import styled from 'styled-components';
+import {
+  DiaryCalminaryAnswer,
+  DiaryDetailEmojiAndDate,
+  DiaryInfoToolTip,
+  DiaryMyWorry,
+} from '../components';
+
 function DiaryDetailPage() {
-  return <div></div>;
+  return (
+    <S_DiaryDetailPage>
+      <DiaryInfoToolTip />
+      <DiaryDetailEmojiAndDate />
+      <DiaryMyWorry />
+      <DiaryCalminaryAnswer />
+    </S_DiaryDetailPage>
+  );
 }
 
 export default DiaryDetailPage;
+
+const S_DiaryDetailPage = styled.section`
+  width: 100%;
+  overflow: hidden;
+`;
