05146dcdea0308ab9669710364f0eb277375c455
Gose <Winter-Climbing@github.com>
2024-10-25 14:10:42 +0900

    feat: info 툴팁 UI 구현 - 말풍선 구현 X

--- src/features/diary/components/DiaryDetailEmojiAndDate.tsx
+++ src/features/diary/components/DiaryDetailEmojiAndDate.tsx
@@ -1,27 +1,29 @@
 import styled from 'styled-components';
 import { Icon } from '../../../components/ui/Icon';
+import logo from '../../../assets/imgs/diary-speech.png';
 
-function DiaryDetailEmojiAndDate() {
+// Styled components
+
+const DiaryDetailEmojiAndDate = () => {
   return (
     <S_DiaryDetailEmojiAndDateWrapper>
-      <Icon type="emoji_smail" size={55} />
+      <div>
+        <Icon type="emoji_smail" size={55} />
+      </div>
       <p>24년 10월 22일의 고민</p>
     </S_DiaryDetailEmojiAndDateWrapper>
   );
-}
+};
 
 export default DiaryDetailEmojiAndDate;
 
 const S_DiaryDetailEmojiAndDateWrapper = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   flex-direction: column;
-  height: 8.33dvh;
+  height: 11.33dvh;
   margin-bottom: 20px;
   font-size: 14px;
-
-  p {
-    margin-top: 18px;
-  }
+  position: relative;
 `;
--- src/features/diary/components/DiaryInfoToolTip.tsx
+++ src/features/diary/components/DiaryInfoToolTip.tsx
@@ -1,11 +1,22 @@
+import styled from 'styled-components';
 import { Icon } from '../../../components/ui/Icon';
 
 function DiaryInfoToolTip() {
   return (
-    <button>
-      <Icon type="diary_info" />
-    </button>
+    <S_toolTipBtn>
+      <Icon type="diary_info" alt="인포메이션 버튼" size={20} />
+    </S_toolTipBtn>
   );
 }
 
 export default DiaryInfoToolTip;
+
+const S_toolTipBtn = styled.button`
+  all: unset;
+
+  position: fixed;
+  right: 30px;
+  z-index: 999;
+  top: 12.33dvh;
+  background-color: ${({ theme }) => theme.colors.brand_bg};
+`;
