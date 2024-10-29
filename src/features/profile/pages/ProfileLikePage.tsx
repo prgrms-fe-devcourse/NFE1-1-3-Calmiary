94280419733ea0abb805bb410514653ac5a46dd1
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-10-29 16:19:58 +0900

    feat: 좋아요포스트 데이터 연결

--- src/features/profile/pages/ProfileLikePage.tsx
+++ src/features/profile/pages/ProfileLikePage.tsx
@@ -1,50 +1,50 @@
 import styled from 'styled-components';
 import LikePost from '../components/LikePost';
 import Dropdown from '../components/Dropdown';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface PostPropTypes {
+  id: number;
+  content: string;
+  like_count: number;
+  created_at: string;
+  nickname: string;
+  comment_count: number;
+}
 
 export default function ProfileLikePage() {
-  const [sortOption, setSortOption] = useState('최신순');
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [sortOption, setSortOption] = useState<
+    '최신순' | '좋아요순' | '오래된순'
+  >('최신순');
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+  const [likedPosts, setLikedPosts] = useState<PostPropTypes[]>([]);
 
-  const posts = [
-    {
-      id: 1,
-      content: 'Post 1',
-      likes: 3,
-      createdAt: '2023-10-21',
-      nickname: '사람1',
-      comments: 4,
-    },
-    {
-      id: 2,
-      content: 'Post 2',
-      likes: 10,
-      createdAt: '2023-10-19',
-      nickname: '사람2',
-      comments: 11,
-    },
-    {
-      id: 3,
-      content: 'Post 3',
-      likes: 5,
-      createdAt: '2023-10-18',
-      nickname: '사람3',
-      comments: 7,
-    },
-  ];
+  useEffect(() => {
+    const fetchLikedPosts = async () => {
+      try {
+        let sortBy = '';
+        if (sortOption === '최신순') {
+          sortBy = 'latest';
+        } else if (sortOption === '좋아요순') {
+          sortBy = 'likes';
+        } else if (sortOption === '오래된순') {
+          sortBy = 'comments';
+        }
+        const response = await axios.get(
+          `https://calmiary-be.org/profile/posts/liked/1?sort_by=${sortBy}&page=1&limit=5`
+        );
 
-  const sortedPosts = [...posts].sort((firstPost, secondPost) => {
-    if (sortOption === '최신순') {
-      return (
-        new Date(secondPost.createdAt).getTime() -
-        new Date(firstPost.createdAt).getTime()
-      );
-    } else if (sortOption === '좋아요순') {
-      return secondPost.likes - firstPost.likes;
-    }
-    return 0;
-  });
+        setLikedPosts(response.data);
+      } catch (error) {
+        console.error('Failed to fetch liked posts:', error);
+      }
+    };
+
+    fetchLikedPosts();
+  }, [sortOption]);
+
+  console.log(likedPosts);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -57,20 +57,22 @@ export default function ProfileLikePage() {
         <DropdownArea>
           <Dropdown
             category={sortOption}
-            data={['최신순', '좋아요순']}
-            setState={setSortOption}
+            data={['최신순', '좋아요순', '오래된순']}
+            setState={
+              setSortOption as React.Dispatch<React.SetStateAction<string>>
+            }
             isOpen={isDropdownOpen}
             setSelectedDropDown={toggleDropdown}
           />
         </DropdownArea>
-        {sortedPosts.map((post) => (
+        {likedPosts.map((post) => (
           <LikePost
             key={post.id}
             content={post.content}
-            likes={post.likes}
-            createdAt={post.createdAt}
+            likes={post.like_count}
+            createdAt={post.created_at}
             nickname={post.nickname}
-            comments={post.comments}
+            comments={post.comment_count}
           />
         ))}
       </ProfileContainer>
