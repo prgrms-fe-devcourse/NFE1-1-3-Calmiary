04550200bfc134074d0c7d0f7ede3de0b1b4f312
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-11-04 15:19:25 +0900

    fix: 회원탈퇴 mutation 수정

--- src/features/profile/hook/useWithdrawMutation.ts
+++ src/features/profile/hook/useWithdrawMutation.ts
@@ -1,26 +1,25 @@
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { useUser } from '../../home/hooks/useUser';
 
 interface WithdrawError {
   message: string;
 }
 
-export const useWithdrawMutation = () => {
-  const { getUserId } = useUser();
-  const navigate = useNavigate();
+interface UseWithdrawMutationOptions {
+  onSuccess?: () => void;
+  onError?: (error: WithdrawError) => void;
+}
 
+export const useWithdrawMutation = (options?: UseWithdrawMutationOptions) => {
   return useMutation({
-    mutationFn: async () => {
-      const { user_id } = getUserId();
-      await axios.delete(`/api/profile/withdraw/${user_id}`);
+    mutationFn: async (userId: string) => {
+      await axios.delete(`/api/profile/withdraw/${userId}`);
     },
     onSuccess: () => {
-      navigate('/login');
+      options?.onSuccess?.();
     },
     onError: (error: WithdrawError) => {
-      alert(`오류 발생: ${error.message}`); //toast로 바꾸기
+      options?.onError?.(error);
     },
   });
 };
