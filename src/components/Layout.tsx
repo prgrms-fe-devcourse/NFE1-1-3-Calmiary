e0ab58053048d10311a85a31f383a6b7af2b0d69
minjeongss <dotoriido@gmail.com>
2024-11-05 11:24:35 +0900

    style: Layout에서 main 삭제 - #56

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,9 +5,7 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <MobileWrapper>
-      <main>
-        <Outlet />
-      </main>
+      <Outlet />
       <Navbar />
     </MobileWrapper>
   );
